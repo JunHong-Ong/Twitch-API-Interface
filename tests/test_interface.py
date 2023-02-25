150dc3a899e54edc09728c292eeebb657b646c59
Ong Jun Hong <53672346+JunHong-Ong@users.noreply.github.com>
2023-02-25 14:27:48 +0800

    Formatted .py files

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,7 +1,12 @@
+""" This modules implments various tests used in the CI pipeline.
+    The test includes:
+        - Check functionality of requesting app access token
+"""
+
 import json
-from requests import Response
 from unittest import TestCase
 from unittest.mock import patch, Mock
+from requests import Response
 
 from twitch.interface import HelixInterface, Token
 
@@ -10,20 +15,20 @@ HELIX_DATA = {}
 def setUpModule():
     """Load Helix responses needed for tests"""
     global HELIX_DATA
-    with open('tests/fixtures/helix_token_response.json') as json_data:
+    with open('tests/fixtures/helix_token_response.json', encoding='utf-8') as json_data:
         HELIX_DATA = json.load(json_data)
 
 class TestAuthorization(TestCase):
     """Test cases for requesting an app access token for authorization"""
-    
+
     ######################################################################
     #  T E S T   C A S E S
-    ######################################################################    
+    ######################################################################
 
     @patch('requests.request')
     def test_valid_credentials(self, interface_mock):
         """Test getting an app access token with valid credentials"""
-        
+
         interface_mock.return_value = Mock(spec=Response,
                                            status_code=200)
         interface_mock.return_value.json.return_value = HELIX_DATA["TOKEN"]["GOOD_REQUEST"]
@@ -33,42 +38,42 @@ class TestAuthorization(TestCase):
         self.assertIsInstance(interface.token, Token)
         self.assertEqual(interface.token.value, "jostpf5q0uzmxmkba9iyug38kjtgh")
         self.assertEqual(str(interface.token), "Bearer jostpf5q0uzmxmkba9iyug38kjtgh")
-    
+
     @patch('requests.request')
     def test_invalid_client_id(self, interface_mock):
         """Test getting an app access token with invalid client id"""
-        
+
         interface_mock.return_value = Mock(spec=Response,
                                            status_code=400)
         interface_mock.return_value.json.return_value = HELIX_DATA["TOKEN"]["INVALID_ID"]
 
-        with self.assertRaises(ValueError) as cm:
+        with self.assertRaises(ValueError) as exp:
             HelixInterface("test_id", "test_secret")
-        
-        self.assertEqual(str(cm.exception), "invalid client")
-    
+
+        self.assertEqual(str(exp.exception), "invalid client")
+
     @patch('requests.request')
     def test_invalid_client_secret(self, interface_mock):
         """Test getting an app access token with invalid client secret"""
-        
+
         interface_mock.return_value = Mock(spec=Response,
                                            status_code=403)
         interface_mock.return_value.json.return_value = HELIX_DATA["TOKEN"]["INVALID_SECRET"]
 
-        with self.assertRaises(ValueError) as cm:
+        with self.assertRaises(ValueError) as exp:
             HelixInterface("test_id", "test_secret")
-        
-        self.assertEqual(str(cm.exception), "invalid client secret")
+
+        self.assertEqual(str(exp.exception), "invalid client secret")
 
     @patch('requests.request')
     def test_invalid_grant_type(self, interface_mock):
         """Test getting an app access token with invalid grant type"""
-        
+
         interface_mock.return_value = Mock(spec=Response,
                                            status_code=400)
         interface_mock.return_value.json.return_value = HELIX_DATA["TOKEN"]["INVALID_GRANT_TYPE"]
 
-        with self.assertRaises(ValueError) as cm:
+        with self.assertRaises(ValueError) as exp:
             HelixInterface("test_id", "test_secret")
-        
-        self.assertEqual(str(cm.exception), "invalid grant type")
+
+        self.assertEqual(str(exp.exception), "invalid grant type")
--- twitch/interface.py
+++ twitch/interface.py
@@ -7,6 +7,8 @@ from typing import Union
 import logging
 import requests
 
+logger = logging.getLogger(__name__)
+
 class Token():
     """The Token class stores data received from the OAuth Client Credentials Flow"""
 
@@ -22,11 +24,15 @@ class Token():
         return self._data.get("access_token")
 
 class HelixInterface():
+    """ The HelixInterface class allow users to send and receive HTTP requests
+        and responses from the Twitch Helix API. 
+    """
 
     def __init__(self, client_id: str, client_secret: str) -> None:
         self.token = self.get_app_access_token(client_id, client_secret)
 
     def get_app_access_token(self, client_id: str, client_secret: str) -> Token:
+        """ Requests an app access token from the oauth endpoint. """
 
         params = {
             "client_id":client_id,
@@ -34,9 +40,10 @@ class HelixInterface():
             "grant_type":"client_credentials"
         }
 
-        response = requests.request("POST", "https://id.twitch.tv/oauth2/token", params=params)
+        response = requests.request("POST", "https://id.twitch.tv/oauth2/token", params=params,
+                                        timeout=5)
 
         if response.status_code == 200:
             return Token(response.json())
-        
+
         raise ValueError(response.json().get("message"))
