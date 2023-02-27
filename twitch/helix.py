53e41f340e7cb609661396f3f161e481810aaada
Ong Jun Hong <53672346+JunHong-Ong@users.noreply.github.com>
2023-02-27 11:26:12 +0800

    added get channels

--- twitch/helix.py
+++ twitch/helix.py
@@ -5,7 +5,7 @@ from typing import Union, Generator
 from warnings import warn
 from .interface import HelixInterface
 
-from .helix_resources import Video
+from .helix_resources import Video, Channel
 
 class Helix():
     """TODO: INSERT CLASS DOCSTRING"""
@@ -41,7 +41,6 @@ class Helix():
         """
 
         endpoint = "videos"
-        print(video_ids)
 
         # Check that only str and int are provided
         wrong_types = [ str(video_id) for video_id in video_ids
@@ -52,7 +51,6 @@ class Helix():
         # Check that all strings are numeric
         non_numeric = [ str(video_id) for video_id in video_ids
                        if isinstance(video_id, str) and not video_id.isnumeric() ]
-        print(non_numeric)
         if len(non_numeric) != 0:
             raise ValueError(f"Video IDs: {', '.join(non_numeric)} are non-numeric.")
 
@@ -66,14 +64,40 @@ class Helix():
         response = self.interface.get(endpoint, headers=self.interface.headers, params=params)
 
         # Inform users of IDs which were not found
-        ids_found = { str(data["id"]) for data in response.json()["data"] }
+        ids_found = { str(data["id"]) for data in response["data"] }
         ids_not_found = { str(video_id) for video_id in video_ids } - ids_found
         if len(ids_not_found) != 0:
             warn(f"Video IDs: {', '.join(ids_not_found)} could not be found.")
 
-        for video_data in response.json()["data"]:
+        for video_data in response["data"]:
             yield Video(video_data)
 
-    def get_channels(self):
-        """TODO: INSERT DOCSTRING, Implement"""
-        raise NotImplementedError
+    def get_channels(self, *broadcaster_ids: Union[str, int]):
+        """TODO: INSERT DOCSTRING"""
+
+        endpoint = "channels"
+
+        # Check that only str and int are provided
+        wrong_types = [ str(broadcaster_id) for broadcaster_id in broadcaster_ids
+                       if type(broadcaster_id) not in [str, int] ]
+        if len(wrong_types) != 0:
+            raise TypeError(
+                f"Broadcaster IDs: {', '.join(wrong_types)} are not of type str or int.")
+
+        # Check that all strings are numeric
+        non_numeric = [ str(broadcaster_id) for broadcaster_id in broadcaster_ids
+                       if isinstance(broadcaster_id, str) and not broadcaster_id.isnumeric() ]
+        if len(non_numeric) != 0:
+            raise ValueError(f"Broadcaster IDs: {', '.join(non_numeric)} are non-numeric.")
+
+        # Chunks broadcaster_ids into batches of max 100
+        if len(broadcaster_ids) <= 100:
+            params = {"broadcaster_id": broadcaster_ids}
+        else:
+            params = {"broadcaster_id": broadcaster_ids[:100]}
+            yield from self.get_channels(*broadcaster_ids[100:])
+
+        response = self.interface.get(endpoint, headers=self.interface.headers, params=params)
+
+        for channel_data in response["data"]:
+            yield Channel(channel_data)
